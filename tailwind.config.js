eb8cc815d5a1c21e814ec322a91de9cdd009129a
Mohammed Seyam <seyam.ms@gmail.com>
2024-10-04 21:57:02 +0300

    add services page and single service page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ import forms from "@tailwindcss/forms";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html", // don't forget to add other files and directories
+    "./*.html", // don't forget to add other files and directories
+    "./*/*.html", // don't forget to add other files and directories
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,25 @@
-export default {
-  base: "https://seyamms.github.io/almonasib/",
+import { resolve } from "path";
+// import { env } from "process";
+import { defineConfig, loadEnv } from "vite";
+
+export default ({ mode }) => {
+  // console.log(mode);
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  // console.log(loadEnv(mode, process.cwd()));
+  // console.log(process.env);
+
+  return defineConfig({
+    base: process.env.VITE_BASE_URL,
+
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          services: resolve(__dirname, "services/index.html"),
+          service: resolve(__dirname, "service/index.html"),
+        },
+      },
+    },
+  });
 };
